ea614d2580fce900626a784c5529a9e227568f0c
Tatiana Cuadros <cuadrost31@gmail.com>
2025-02-17 20:27:09 -0500

    Creando interfaz grafica de usuario

---
+++ src/FrmJuego.java
@@ -0,0 +1,72 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+import javax.swing.WindowConstants;
+
+public class FrmJuego extends JFrame{
+
+    JPanel pnlJugador1, pnlJugador2;
+
+    public FrmJuego(){
+        setTitle("Jueguemos al Apuntado!!");
+        setSize(700, 200);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLayout(null);
+
+        JButton btnRepartir = new JButton("Repartir");
+        btnRepartir.setBounds(10, 10, 100, 25);
+        getContentPane().add(btnRepartir);
+
+        JButton btnVerificar = new JButton("Verificar");
+        btnVerificar.setBounds(120, 10, 100, 25);
+        getContentPane().add(btnVerificar);
+
+        JTabbedPane tpJugadores = new JTabbedPane();
+        tpJugadores.setBounds(10, 40, 656, 150);
+        getContentPane().add(tpJugadores);
+
+        pnlJugador1 = new JPanel();
+        pnlJugador1.setBackground(new Color(16, 139, 37));
+        pnlJugador1.setLayout(null);
+
+        pnlJugador2 = new JPanel();
+        pnlJugador2.setBackground(new Color(0, 255, 255));
+        pnlJugador2.setLayout(null);
+
+        tpJugadores.addTab("Andrés Gutierréz", pnlJugador1);
+        tpJugadores.addTab("Tatiana Cuadros", pnlJugador2);
+
+        btnRepartir.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                repartirCartas();
+            }
+        });
+
+        btnVerificar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                verificarJugador();
+            }
+        });
+    }
+
+    // Jugador jugador1 = new Jugador();
+    // Jugador jugador2 = new Jugador();
+
+    private void repartirCartas() {
+        // jugador1.repartir();
+        // jugador1.mostrar(pnlJugador1);
+        // jugador2.repartir();
+        // jugador2.mostrar(pnlJugador2);
+    }
+
+    private void verificarJugador() {
+
+    }
+}
